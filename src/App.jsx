7834fac1c19e349023b05f14fcd22b3dab5179b9
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2025-01-12 17:37:28 +0200

    fox1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Component } from 'react';
 import { ToastContainer } from 'react-toastify';
 import PokemonForm from './components/PokemonForm';
-// import PokemonInfo from './components/PokemonInfo';
+import PokemonInfo from './components/PokemonInfo';
 
 export default class App extends Component {
   state = {
@@ -15,6 +15,7 @@ export default class App extends Component {
     return (
       <div style={{ maxWidth: 1170, margin: '0 auto', padding: 20 }}>
         <PokemonForm onSubmit={this.handleFormSubmit} />
+        <PokemonInfo pokemonName={this.state.pokemonName} />
         <ToastContainer autoClose={3000} />
       </div>
     );
--- src/components/PokemonInfo.js
+++ src/components/PokemonInfo.js
@@ -1,24 +1,31 @@
 import { Component } from 'react';
-import PokemonDataView from './PokemonDataView';
-import PokemonErrorView from './PokemonErrorView';
-import PokemonPendingView from './PokemonPendingView';
-import pokemonAPI from '../services/pokemon-api';
+// import PokemonDataView from './PokemonDataView';
+// import PokemonErrorView from './PokemonErrorView';
+// import PokemonPendingView from './PokemonPendingView';
+// import pokemonAPI from '../services/pokemon-api';
 
-const Status = {
-  IDLE: 'idle',
-  PENDING: 'pending',
-  RESOLVED: 'resolved',
-  REJECTED: 'rejected',
-};
+// const Status = {
+//   IDLE: 'idle',
+//   PENDING: 'pending',
+//   RESOLVED: 'resolved',
+//   REJECTED: 'rejected',
+// };
 
 export default class PokemonInfo extends Component {
-  state = {
-    pokemon: null,
-    error: null,
-    status: Status.IDLE,
-  };
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.pokemonName !== this.props.pokemonName) {
+      console.log('Изменилось имя покемона');
+      console.log('prevProps.pokemonName: ', prevProps.pokemonName);
+      console.log('this.props.pokemonName: ', this.props.pokemonName);
+    }
+  }
 
   render() {
-    return <div>qweqqwe</div>;
+    return (
+      <div>
+        <h1>PokemonInfo</h1>
+        <p>{this.props.pokemonName}</p>
+      </div>
+    );
   }
 }
